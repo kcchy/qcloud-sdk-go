6f72ad76f5379e14d14baa6acb7bd3d00c347f91
Damon Kong <damon.kong9@gmail.com>
2018-07-17 09:37:35 +0800

    Add func DescribeBaseMetrics to get metrcs list and fixed typo

--- monitor/gaap.go
+++ monitor/gaap.go
@@ -4,6 +4,35 @@ import (
 	"github.com/kcchy/qcloud-sdk-go/common"
 )
 
+type DescribeBaseMetricsArgs struct {
+	Namespace  string
+	MetricName string
+}
+
+type DescribeBaseMetricsResponse struct {
+	Code      int    `json:"code"`
+	Message   string `json:"message"`
+	MetricSet []struct {
+		Dimensions  string `json:"dimensions"`
+		MetricCname string `json:"metricCname"`
+		MetricName  string `json:"metricName"`
+		Namespace   string `json:"namespace"`
+		Period      []int  `json:"period"`
+		Unit        string `json:"unit"`
+		UnitCname   string `json:"unitCname"`
+	} `json:"metricSet"`
+}
+
+func (client *Client) DescribeBaseMetrics(args *DescribeBaseMetricsArgs) (response *DescribeBaseMetricsResponse, err error) {
+	response = &DescribeBaseMetricsResponse{}
+
+	err = client.Invoke("DescribeBaseMetrics", args, response)
+	if err != nil {
+		return nil, err
+	}
+	return response, err
+}
+
 type GetMonitorDataArgs struct {
 	Namespace  string
 	MetricName string
@@ -13,17 +42,17 @@ type GetMonitorDataArgs struct {
 	Dimensions common.FlattenMultiDimensionalArray
 }
 
-type GetMonitorDataReponse struct {
+type GetMonitorDataResponse struct {
 	common.CommonResponse
-	StartTime  string `json:"startTime"`
-	EndTime    string `json:"endTime"`
-	MetricName string `json:"metricName"`
-	Period     int    `json:"period"`
-	DataPoints []int  `json:"dataPoints"`
+	StartTime  string    `json:"startTime"`
+	EndTime    string    `json:"endTime"`
+	MetricName string    `json:"metricName"`
+	Period     int       `json:"period"`
+	DataPoints []float64 `json:"dataPoints"`
 }
 
-func (client *Client) GetMonitorData(args *GetMonitorDataArgs) (response *GetMonitorDataReponse, err error) {
-	response = &GetMonitorDataReponse{}
+func (client *Client) GetMonitorData(args *GetMonitorDataArgs) (response *GetMonitorDataResponse, err error) {
+	response = &GetMonitorDataResponse{}
 
 	err = client.Invoke("GetMonitorData", args, response)
 	if err != nil {
